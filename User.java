362dfc11fcb33348e032298205223dc689dcd788
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-10-23 14:59:09 +0200

    Update User.java

--- User.java
+++ User.java
@@ -3,21 +3,20 @@ public class User {
 	 String name;
 	 String nick;
 	 String password;
-	 String birthday;
 	 String phonenumber;
 	 boolean premium;
 	 
 	 public User(String name,
 			   String nick,
 			   String password,
-			   String birthday,
+			
 			   String phonenumber,
 			   boolean premium
 			   ) {
 			this.name = name;
 			this.nick = nick;
 			this.password = password;
-			this.birthday = birthday;
+		
 			this.phonenumber= phonenumber;
 		    this.premium = false;
 			}
@@ -31,9 +30,7 @@ return this.nick;
 public String getPassword() {
 return this.password;
 }
-public String getBirthday() {
-return this.birthday;
-}
+
 public String getphonenumber() {
 return this.phonenumber;
 }
@@ -43,8 +40,7 @@ public boolean getPremium() {
 
 public String getData() {
 return "Name: " + this.name + 
-" Nick: " + this.nick + "\t" +
-"Birthday: " + this.birthday + "\t" +
+" Nick: " + this.nick + "\t" + 
 "Phonenumber: " + this.phonenumber +
 "Premium user: " + this.premium;
 
