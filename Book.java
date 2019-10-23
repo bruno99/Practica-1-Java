4c1670c7872c12d96daa684732fd978504455e9d
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-10-23 14:24:08 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,55 +1,25 @@
 package practica1;
 
-public class Book {
- String name;
- String author;
- String year;
+public class Book extends Product {
+ 
  String editorial;
- int amount;
- boolean available;
+ 
 
 
 public Book(String name,
-   String author,
-   String year,
-   String editorial
+		   String author,
+		   String year,
+		   int amount,
+		   boolean available,
+           String editorial
    ) {
-this.name = name;
-this.author = author;
-this.year = year;
+super(name, author, year, amount, available);
 this.editorial= editorial;
-this.amount = 1;
-this.available = true;
-}
-public String getName() {
-return this.name;
-}
-public String getAuthor() {
-return this.author;
-}
-public String getYear() {
-return this.year;
 }
 public String getEditorial() {
-return this.editorial;
-}
-public int getAmount() {
-return this.amount;
-}
-public void setAmount(int amount) {
-this.amount = amount;
-}
-public boolean getAvailable() {
-return this.available;
-}
-public void setAvailable(boolean available) {
-	if (this.amount > 0)		
-this.available = true;
-	else {
-		this.available = false;
-	}	
-}
-public String getData() {
+	 return this.editorial;
+	 }
+public String getBData() {
 return "Name: " + this.name + "\t" +
 "Author: " + this.author + "\t" +
 "Year: " + this.year + "\t" +
@@ -57,5 +27,5 @@ return "Name: " + this.name + "\t" +
 "Number of books available: " + this.amount;
 
 }
-
 }
+	 
