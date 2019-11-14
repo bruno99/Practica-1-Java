cc660099a9859f7d0dd81dc50b0678cf5ae51c85
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-11-14 20:26:45 +0100

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,31 +1,55 @@
 package practica1;
 
+
+
 public class Book extends Product {
- 
+
+
+
  String editorial;
- 
+
 
 
 public Book(String name,
+
 		   String author,
+
 		   String year,
+
 		   int amount,
+
 		   boolean available,
+
            String editorial
+
    ) {
+
 super(name, author, year, amount, available);
+
 this.editorial= editorial;
-}
-public String getEditorial() {
-	 return this.editorial;
-	 }
-public String getBData() {
-return "Name: " + this.name + "\t" +
-"Author: " + this.author + "\t" +
-"Year: " + this.year + "\t" +
-"Editorial: " + this.editorial + "\t" +
-"Number of books available: " + this.amount;
 
 }
+
+public String getEditorial() {
+
+	 return this.editorial;
+
+	 }
+
+public String getBData() {
+
+return "Name: " + this.name + "\t" +
+
+"Author: " + this.author + "\t" +
+
+"Year: " + this.year + "\t" +
+
+"Editorial: " + this.editorial + "\t" +
+
+"Number of books available: " + this.amount;
+
+
+
 }
-	 
+
+}
