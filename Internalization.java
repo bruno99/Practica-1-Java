863813a1ebf0846b2d86206d175102078591e47b
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-11-14 20:27:33 +0100

    Create Internalization.java

---
+++ Internalization.java
@@ -0,0 +1,38 @@
+package practica1;
+import java.util.Locale;
+
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+
+
+public class Internalization {
+	static Scanner sn = new Scanner(System.in);
+    private static Locale locale;
+    static Locale esLocale = new Locale("es", "ES");
+    static Locale enLocale = new Locale("en", "US");
+   
+
+    
+    public static Locale setLocale() {
+    	
+    	System.out.println(" 1. English.\n 2. Español");
+    	int option = sn.nextInt();
+    	switch(option) {
+    		case 1:
+    			locale = Internalization.enLocale;
+    			break;
+    		case 2:
+    			locale = Internalization.esLocale;
+    			break;
+  
+    		default:
+    			locale = Internalization.esLocale;
+    	}  
+    	return locale;
+    }
+
+    public static String getString(String key) {
+        return ResourceBundle.getBundle("MessagesBundle", locale).getString(key);
+    }
+}
