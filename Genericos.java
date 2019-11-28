538470ba43f2fa01f6c600c5ffd5a5264c6aa58e
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-11-28 10:12:42 +0100

    Create Genericos.java

---
+++ Genericos.java
@@ -0,0 +1,13 @@
+public class Genericos<T> {
+private T valor;
+
+public Genericos(T valor){
+this.valor = valor;
+}
+public T getvalor(){
+return this.valor;
+}
+public void setValor(T valor){
+this.valor = valor;
+}
+}
