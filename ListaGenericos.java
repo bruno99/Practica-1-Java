f8d115f450932e64b6082d87ae209a2fdd34f2ee
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-11-28 10:30:50 +0100

    Create ListaGenericos.java

---
+++ ListaGenericos.java
@@ -0,0 +1,13 @@
+public class ListaGenericos <T extends Generico<T>>{
+private <T> vector;
+
+public Lista (T[] vector){
+this.vector = vector;
+}
+public T ListG(){
+T ListG = this.vector[0];
+
+for(int i=1; i<this.vector.length; i++){
+System.out.println(this.vector[i].getValor());
+}
+
