a063923d2ef2ef99145b8b635f89360e151b97cf
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-12-04 10:31:27 +0100

    Create LoteriaGanadora.java

---
+++ LoteriaGanadora.java
@@ -0,0 +1,30 @@
+package practica1;
+
+
+int[][] loteriaGanadora = [3][3];
+public int setData(){
+
+ for(int i=0; i< loteriaGanadora.length; i++)
+
+    for(int j=0; j< loteriaGanadora.length; j++)
+
+          loteriaGanadora[i][j] = Math.floor(Math.random()*(9+1);
+} 
+public int getData(){
+
+ for(int i=0; i< loteriaGanadora.length; i++)
+
+    for(int j=0; j< loteriaGanadora.length; j++)
+
+         System.out.println(loteriaGanadora[i][j]) + " ";
+
+} 
+
+public String getGanador(){
+if(loteria.equals(loteriaGanadora)){
+  System.out.println("HAS GANADO 100$");
+  cuenta= cuenta-100;  
+}  else{
+System.out.println(SIGUE PROBANDO");
+}
+}
