5e44abe41b0ee0f098458522f5da1410bc1734d6
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-12-10 17:44:02 +0100

    Create Bubble.java

---
+++ Bubble.java
@@ -0,0 +1,23 @@
+
+package practica1;
+
+public class Bubble{
+int menor;
+
+    for(int i = 0; i < 10; i++){
+        menor = vector[0].getYear();
+
+        if (vector[i] < menor){
+            menor = vector[i].getYear();
+        }
+        else{
+            if (vector[i].getYear() > menor){
+              menor = menor;
+            }      
+        }
+    }
+    for(int i=1; i<this.vector.length; i++){
+System.out.println(this.vector[i].getValor());
+}
+}
+}
