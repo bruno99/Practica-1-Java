3d815675841bcac7c4d9e4d8fcda04ae821eed15
bruno99 <43136694+bruno99@users.noreply.github.com>
2019-12-10 18:39:13 +0100

    Create Cuenta.java

---
+++ Cuenta.java
@@ -0,0 +1,38 @@
+package practica 1;
+
+public class Cuenta {
+ private double saldo;
+ public Cuenta(double saldo) {
+ this.saldo = saldo;
+ }
+ public double getSaldo() {
+ return this.saldo;
+ }
+ public void deposito(double importe) {
+ double nuevoSaldo = this.saldo;
+ nuevoSaldo = nuevoSaldo + importe;
+ try {
+ Thread.sleep(300); 
+ } catch (InterruptedException ie) {
+ System.err.println(ie.getMessage());
+ }
+ System.out.println("el saldo después del depósito " + nuevoSaldo);
+ this.saldo = nuevoSaldo;
+ }
+ 
+ public void retiro(double importe) {
+ if (this.saldo < importe) {
+ System.out.println("No hay saldo suficiente!");
+ return;
+ }
+ double nuevoSaldo = this.saldo;
+ nuevoSaldo = nuevoSaldo - importe;
+ try {
+ Thread.sleep(200);
+ } catch (InterruptedException ie) {
+ System.err.println(ie.getMessage());
+ }
+ System.out.println("el saldo después del retiro " + nuevoSaldo);
+ this.saldo = nuevoSaldo;
+ }
+}
